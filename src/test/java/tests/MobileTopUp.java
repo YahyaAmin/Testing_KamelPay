ad9bd83335fdc288389f5e86629559e832f581ec
YahyaAmin <yahyaamin1997@gmail.com>
2022-06-09 19:18:11 +0500

    Change

--- src/test/java/tests/MobileTopUp.java
+++ src/test/java/tests/MobileTopUp.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import static tests.UsefulFunctions.getRandomNumberLowerAndUpperBound;
 
 
-public class MobileTopUp extends Base_Class{
+public class MobileTopUp extends Base_Class {
 
     public String getRandomString(boolean Digits, boolean Lower, boolean Upper, boolean Punctuation, boolean Spaces, int length) {
 
@@ -33,13 +33,12 @@ public class MobileTopUp extends Base_Class{
 
     public String new_topup_button = "//android.widget.TextView[@text='New Topup']";
 
-    public String country_selector_at_phone_number = "//android.widget.TextView[@text='+971']";
+    public String country_selector_at_phone_number = "//android.widget.ImageView[@index='0']";
 
     public String search_country_field = "//android.widget.EditText[@text='Search']";
 
-    public String getCountryOfPhoneNumber(String countrie)
-    {
-        return "//android.widget.TextView[@text='"+countrie+"']";
+    public String getCountryOfPhoneNumber(String countrie) {
+        return "//android.widget.TextView[@text='" + countrie + "']";
     }
 
     public String enter_phone_number_mobiletopup = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText";
@@ -56,6 +55,13 @@ public class MobileTopUp extends Base_Class{
 
     public String pay_button_at_top_up_screen = "//android.widget.TextView[@text='Pay']";
 
+    public String first_beneficiary_selector_at_top_up = "//android.view.ViewGroup[@index='0']";
+
+    public String first_recharge_option_mobile_topup = "//android.view.ViewGroup[@index='7']";
+
+    public String search_bar_at_mobile_topup_screen = "//android.widget.EditText[@index='1']";
+
+    public String search_field_at_service_provider_screen = "//android.widget.EditText[@index='1']";
 
 
 
@@ -63,7 +69,7 @@ public class MobileTopUp extends Base_Class{
     @Order(1)
     public void mobileTopup1() throws InterruptedException {
 
-        String country = "United Arab Emir";
+        //String country = "United Arab Emir";
 
         //login steps
         Thread.sleep(5000);
@@ -94,11 +100,11 @@ public class MobileTopUp extends Base_Class{
         //driver.findElement(By.xpath(getCountryOfPhoneNumber(country))).click();
         //Thread.sleep(2000);
 
-        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(11,13));
+        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(11, 13));
         Thread.sleep(3000);
         driver.findElement(By.xpath(add_account_to_saved_list_checkbox)).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).sendKeys(getRandomString(false,true,true,false,true,10));
+        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).sendKeys(getRandomString(false, true, true, false, true, 10));
         Thread.sleep(2000);
         driver.findElement(By.xpath(next_button_at_topup_screen)).click();
         Thread.sleep(5000);
@@ -149,11 +155,281 @@ public class MobileTopUp extends Base_Class{
         //driver.findElement(By.xpath(getCountryOfPhoneNumber(country))).click();
         //Thread.sleep(2000);
 
-        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(3,6));
+        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(3, 6));
         Thread.sleep(3000);
         driver.findElement(By.xpath(add_account_to_saved_list_checkbox)).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).sendKeys(getRandomString(false,true,true,false,true,10));
+        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).sendKeys(getRandomString(false, true, true, false, true, 10));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+
+        //logged in
+        //close driver
+        driver.resetApp();
+
+    }
+
+
+    @Test(priority = 3) //Pay Bill but leave beneficiary empty
+    @Order(3)
+    public void mobileTopup3() throws InterruptedException {
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(new_topup_button)).click();
+        Thread.sleep(5000);
+
+        //country selector steps
+        //driver.findElement(By.xpath(country_selector_at_phone_number)).click();
+        //Thread.sleep(4000);
+        //driver.findElement(By.xpath(search_country_field)).sendKeys(country);
+        //Thread.sleep(1000);
+        //driver.findElement(By.xpath(getCountryOfPhoneNumber(country))).click();
+        //Thread.sleep(2000);
+
+        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(10, 11));
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(add_account_to_saved_list_checkbox)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+
+
+        //logged in
+        //close driver
+        driver.resetApp();
+
+    }
+
+
+    @Test(priority = 4) //Pay Bill check whether user can change countries
+    @Order(4)
+    public void mobileTopup4() throws InterruptedException {
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(new_topup_button)).click();
+        Thread.sleep(5000);
+
+        //check whether user can access these countries
+        String[] countries = {"Pakistan", "India", "Bangladesh", "Sri Lanka"};
+
+
+        //country selector steps
+        for (int i = 0; i < countries.length; i++) {
+            driver.findElement(By.xpath(country_selector_at_phone_number)).click();
+            Thread.sleep(4000);
+            driver.findElement(By.xpath(search_country_field)).sendKeys(countries[i]);
+            Thread.sleep(1000);
+            driver.findElement(By.xpath(getCountryOfPhoneNumber(countries[i]))).click();
+            Thread.sleep(4000);
+        }
+
+        //logged in
+        //close driver
+        driver.resetApp();
+
+    }
+
+
+    @Test(priority = 5) //Pay Bill check whether user can pay to an existing beneficiary
+    @Order(5)
+    public void mobileTopup5() throws InterruptedException {
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(first_beneficiary_selector_at_top_up)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(first_recharge_option_mobile_topup)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(pay_button_at_top_up_screen)).click();
+        Thread.sleep(10000);
+
+
+        //logged in
+        //close driver
+        driver.resetApp();
+
+    }
+
+
+    @Test(priority = 6) //Pay Bill check whether user can search for beneficiaries
+    @Order(6)
+    public void mobileTopup6() throws InterruptedException {
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(3000);
+
+        String[] beneficiaries = {"yahya", "wasif", "khizer"};
+
+        //beneficiaries search steps
+        for (int i = 0; i < beneficiaries.length; i++) {
+            driver.findElement(By.xpath(search_bar_at_mobile_topup_screen)).sendKeys(beneficiaries[i]);
+            Thread.sleep(3000);
+            driver.findElement(By.xpath(search_bar_at_mobile_topup_screen)).clear();
+            Thread.sleep(3000);
+        }
+
+        //logged in
+        //close driver
+        driver.resetApp();
+
+    }
+
+
+    @Test(priority = 7) //Pay Bill positive check if user can search for service providers
+    public void mobileTopup7() throws InterruptedException {
+
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(new_topup_button)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(11, 13));
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(next_button_at_topup_screen)).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(search_field_at_service_provider_screen)).sendKeys("Eti");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(search_field_at_service_provider_screen)).clear();
+        driver.findElement(By.xpath(search_field_at_service_provider_screen)).sendKeys("Du");
+        Thread.sleep(3000);
+
+
+        //logged in
+        //close driver
+        driver.resetApp();
+    }
+
+    @Test(priority = 8) //Pay Bill but phone number is empty
+    @Order(8)
+    public void mobileTopup8() throws InterruptedException {
+
+        //login steps
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(phone_no_on_mainscreen_xpath)).sendKeys("123456789");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).clear();
+        driver.findElement(By.xpath(password_on_mainscreen_xpath)).sendKeys("Password123!");
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath(login_button_on_mainscreen)).click();
+        //Thread.sleep(3000);
+
+        //dont allow biometric
+        driver.findElement(By.xpath(dont_allow_biometric_button_xpath)).click();
+        Thread.sleep(4000);
+
+        //Mobile Top up steps
+        driver.findElement(By.xpath(mobile_topup_button_mainscreen)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(new_topup_button)).click();
+        Thread.sleep(5000);
+
+        //country selector steps
+        //driver.findElement(By.xpath(country_selector_at_phone_number)).click();
+        //Thread.sleep(4000);
+        //driver.findElement(By.xpath(search_country_field)).sendKeys(country);
+        //Thread.sleep(1000);
+        //driver.findElement(By.xpath(getCountryOfPhoneNumber(country))).click();
+        //Thread.sleep(2000);
+
+        //driver.findElement(By.xpath(enter_phone_number_mobiletopup)).sendKeys(getRandomNumberLowerAndUpperBound(3, 6));
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath(add_account_to_saved_list_checkbox)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(beneficiary_name_at_topup_screen)).sendKeys(getRandomString(false, true, true, false, true, 10));
         Thread.sleep(2000);
         driver.findElement(By.xpath(next_button_at_topup_screen)).click();
         Thread.sleep(5000);
